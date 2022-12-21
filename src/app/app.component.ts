da14e75519c5346a24f1cb23f8c2d25b8a5ae8fe
Enrique <octepc.97@gmail.com>
2022-12-21 12:18:06 -0500

    worked with components and their renderization in the client view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Enrique';
+  age = 25;
+  img = 'https://picsum.photos/200'
 }
---
+++ src/app/recap.ts
@@ -0,0 +1,18 @@
+const username:string = 'enrigios'
+
+const sum = (a:number, b:number)=>{
+  return a + b;
+}
+
+sum(1,2)
+
+class Person {
+
+  constructor(public age: number, public lastName:string){
+
+  }
+}
+
+const enrique = new Person(25, 'palomino')
+
+enrique.age
