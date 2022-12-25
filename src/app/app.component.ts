746711246c1944041e6485c61eedb4d978d5868e
Enrique <octepc.97@gmail.com>
2022-12-25 14:05:43 -0500

    worked with *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,9 @@ export class AppComponent {
     avatar : 'https://picsum.photos/900'
   }
 
+  names: string[]=['enrique','Diana', 'rodrigo'];
+  newName = ''
+
   toggleButton (){
     this.btnDisabled = !this.btnDisabled;
   }
@@ -33,5 +36,14 @@ export class AppComponent {
   changeName(event: Event){
     const element = event.target as HTMLInputElement;
     this.person.name = element.value
+  }
+
+  addName(){
+    this.names.push(this.newName)
+    this.newName = ''
+  }
+
+  deleteName(index:number){
+    this.names.splice(index,1)
   }
 }
